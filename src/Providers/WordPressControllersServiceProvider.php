ae37f667a595a70480252d2d80659cebacd6fd50
Joe Lambert <joe@joelambert.co.uk>
2017-08-09 20:33:20 +0100

    Add support for Controllers in WordPress templates

---
+++ src/Providers/WordPressControllersServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Rareloop\Lumberjack\Providers;
+
+use Psr\Http\Message\RequestInterface;
+use Rareloop\Lumberjack\Application;
+use Rareloop\Router\Invoker;
+use Rareloop\Router\ResponseFactory;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequestFactory;
+
+class WordPressControllersServiceProvider
+{
+    private $app;
+
+    public function boot(Application $app)
+    {
+        $this->app = $app;
+
+        add_filter('template_include', [$this, 'handleTemplateInclude']);
+    }
+
+    public function handleTemplateInclude($template)
+    {
+        include $template;
+
+        $controller = $this->getControllerClassFromTemplate($template);
+
+        $request = ServerRequestFactory::fromGlobals(
+            $_SERVER,
+            $_GET,
+            $_POST,
+            $_COOKIE,
+            $_FILES
+        );
+
+        $response = $this->handleRequest($request, $controller, 'handle');
+
+        if ($response) {
+            $this->app->shutdown($response);
+        }
+    }
+
+    public function getControllerClassFromTemplate($template)
+    {
+        $controllerName = studly_case(basename($template, '.php')).'Controller';
+
+        $controllerName = apply_filters('lumberjack_controller_name', $controllerName);
+        $controllerNamespace = apply_filters('lumberjack_controller_namespace', 'Theme\\');
+
+        return $controllerNamespace.$controllerName;
+    }
+
+    public function handleRequest(RequestInterface $request, $controllerName, $methodName)
+    {
+        if (!class_exists($controllerName)) {
+            return false;
+        }
+
+        $invoker = new Invoker($this->app);
+        $output = $invoker->setRequest($request)->call([$controllerName, $methodName]);
+        return ResponseFactory::create($output);
+    }
+}
---
+++ tests/Unit/Providers/WordPressControllersServiceProviderTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Rareloop\Lumberjack\Test\Providers;
+
+use \Mockery;
+use Brain\Monkey\Filters;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Rareloop\Lumberjack\Application;
+use Rareloop\Lumberjack\Http\Kernal;
+use Rareloop\Lumberjack\Providers\WordPressControllersServiceProvider;
+use Rareloop\Lumberjack\Test\Unit\BrainMonkeyPHPUnitIntegration;
+use Zend\Diactoros\Response\TextResponse;
+use Zend\Diactoros\ServerRequest;
+
+class WordPressControllersServiceProviderTest extends TestCase
+{
+    use BrainMonkeyPHPUnitIntegration;
+
+    /** @test */
+    public function template_include_filter_is_applied_on_boot()
+    {
+        $app = new Application(__DIR__.'/../');
+        $provider = new WordPressControllersServiceProvider;
+
+        $app->register($provider);
+        $app->boot();
+
+        $this->assertTrue(has_filter('template_include', [$provider, 'handleTemplateInclude']));
+    }
+
+    /** @test */
+    public function handle_template_include_method_includes_the_requested_file()
+    {
+        $this->assertNotContains(__DIR__ . '/includes/single.php', get_included_files());
+
+        $provider = new WordPressControllersServiceProvider;
+        $provider->handleTemplateInclude(__DIR__ . '/includes/single.php');
+
+        $this->assertContains(__DIR__ . '/includes/single.php', get_included_files());
+    }
+
+    /** @test */
+    public function can_get_name_of_controller_from_template()
+    {
+        $provider = new WordPressControllersServiceProvider;
+
+        $mappings = [
+            'Theme\\SingleController' => __DIR__ . '/includes/single.php',
+            'Theme\\SingleEventsController' => __DIR__ . '/includes/single_events.php',
+            'Theme\\SingleRlEventsController' => __DIR__ . '/includes/single_rl_events.php',
+        ];
+
+        foreach ($mappings as $className => $template) {
+            $this->assertSame($className, $provider->getControllerClassFromTemplate($template));
+        }
+    }
+
+    /** @test */
+    public function handle_request_returns_false_if_controller_does_not_exist()
+    {
+        $provider = new WordPressControllersServiceProvider;
+
+        $response = $provider->handleRequest(new ServerRequest, 'Does\\Not\\Exist', 'handle');
+
+        $this->assertFalse($response);
+    }
+
+    /** @test */
+    public function handle_request_returns_response_when_controller_does_exist()
+    {
+        $app = new Application(__DIR__.'/../');
+
+        $provider = new WordPressControllersServiceProvider;
+        $provider->boot($app);
+
+        $response = $provider->handleRequest(new ServerRequest, TestController::class, 'handle');
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    /** @test */
+    public function handle_template_include_will_call_app_shutdown_when_it_has_handled_a_request()
+    {
+        $response = new TextResponse('Testing 123', 404);
+        $app = Mockery::mock(Application::class.'[shutdown]', [__DIR__.'/..']);
+        $app->shouldReceive('shutdown')->times(1)->with($response);
+
+        $provider = Mockery::mock(WordPressControllersServiceProvider::class.'[handleRequest]');
+        $provider->shouldReceive('handleRequest')->once()->andReturn($response);
+        $provider->boot($app);
+
+        $provider->handleTemplateInclude(__DIR__ . '/includes/single.php');
+    }
+
+    /** @test */
+    public function handle_template_include_will_not_call_app_shutdown_when_it_has_not_handled_a_request()
+    {
+        $app = Mockery::mock(Application::class.'[shutdown]', [__DIR__.'/..']);
+        $app->shouldReceive('shutdown')->times(0);
+
+        $provider = Mockery::mock(WordPressControllersServiceProvider::class.'[handleRequest]');
+        $provider->shouldReceive('handleRequest')->once()->andReturn(false);
+        $provider->boot($app);
+
+        $provider->handleTemplateInclude(__DIR__ . '/includes/single.php');
+    }
+}
+
+class TestController
+{
+    public function handle()
+    {
+
+    }
+}
