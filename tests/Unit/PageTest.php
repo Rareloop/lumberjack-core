1ee536bfc21702ff027412cc5bd3281a8b5b9c29
Joe Lambert <joe@joelambert.co.uk>
2017-12-09 13:34:44 +0000

    Add test for Page

---
+++ tests/Unit/PageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rareloop\Lumberjack\Test;
+
+use PHPUnit\Framework\TestCase;
+use Rareloop\Lumberjack\Page;
+use Rareloop\Lumberjack\Test\Unit\BrainMonkeyPHPUnitIntegration;
+
+class PageTest extends TestCase
+{
+
+    /** @test */
+    public function page_class_has_correct_post_type()
+    {
+        $this->assertSame('page', Page::getPostType());
+    }
+}
