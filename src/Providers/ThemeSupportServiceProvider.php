77cbf6beff19688feacfd6f97c7254d5a14346b4
Joe Lambert <joe@joelambert.co.uk>
2018-01-23 17:56:31 +0000

    Fix theme support registration

--- src/Providers/ThemeSupportServiceProvider.php
+++ src/Providers/ThemeSupportServiceProvider.php
@@ -9,10 +9,9 @@ class ThemeSupportServiceProvider extends ServiceProvider
     public function boot(Config $config)
     {
         $themeSupport = $config->get('app.themeSupport', []);
-
         foreach ($themeSupport as $key => $value) {
             if (is_numeric($key)) {
-                add_theme_support($key);
+                add_theme_support($value);
             } else {
                 add_theme_support($key, $value);
             }
