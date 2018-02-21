f3e1ca9898b9ac53c34bd2df4786fc0f201a14e9
Joe Lambert <joe@joelambert.co.uk>
2018-02-21 10:44:09 +0000

    User ErrorLogHandler when path is false

--- src/Providers/LogServiceProvider.php
+++ src/Providers/LogServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Rareloop\Lumberjack\Providers;
 
 use Monolog\Handler\BufferHandler;
+use Monolog\Handler\ErrorLogHandler;
 use Monolog\Handler\HandlerWrapper;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -13,10 +14,28 @@ class LogServiceProvider extends ServiceProvider
     public function register()
     {
         $logger = new Logger('app');
-        $logger->pushHandler(new StreamHandler($this->getLogsPath(), $this->getLogLevel()));
+
+        // If the `path` config is set to false then use the Apache/Nginx error logs
+        if ($this->shouldUseErrorLogHandler()) {
+            $logger->pushHandler(new ErrorLogHandler(ErrorLogHandler::OPERATING_SYSTEM, $this->getLogLevel()));
+        } else {
+            $logger->pushHandler(new StreamHandler($this->getLogsPath(), $this->getLogLevel()));
+        }
 
         $this->app->bind('logger', $logger);
         $this->app->bind(Logger::class, $logger);
+    }
+
+    private function shouldUseErrorLogHandler()
+    {
+        $config = false;
+
+        // Get the config from the container if it's been registered
+        if ($this->app->has('config')) {
+            $config = $this->app->get('config');
+        }
+
+        return $config && $config->get('app.logs.path') === false && $config->get('app.logs.enabled') === true;
     }
 
     private function getLogLevel()
--- tests/Unit/Providers/LogServiceProviderTest.php
+++ tests/Unit/Providers/LogServiceProviderTest.php
@@ -3,6 +3,7 @@
 namespace Rareloop\Lumberjack\Test\Providers;
 
 use Brain\Monkey\Functions;
+use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 use Rareloop\Lumberjack\Application;
@@ -92,6 +93,40 @@ class LogServiceProviderTest extends TestCase
         ]);
 
         $this->assertSame(Logger::ERROR, $app->get('logger')->getHandlers()[0]->getLevel());
+    }
+
+    /** @test */
+    public function error_log_is_used_when_path_is_set_to_false()
+    {
+        $app = new Application('/base/path');
+
+        $config = new Config;
+        $config->set('app.logs.enabled', true);
+        $config->set('app.logs.path', false);
+        $app->bind('config', $config);
+
+        $app->bootstrapWith([
+            RegisterProviders::class,
+        ]);
+
+        $this->assertInstanceOf(ErrorLogHandler::class, $app->get('logger')->getHandlers()[0]);
+    }
+
+    /** @test */
+    public function stream_is_used_when_path_is_set_to_false_and_enabled_is_false()
+    {
+        $app = new Application('/base/path');
+
+        $config = new Config;
+        $config->set('app.logs.enabled', false);
+        $config->set('app.logs.path', false);
+        $app->bind('config', $config);
+
+        $app->bootstrapWith([
+            RegisterProviders::class,
+        ]);
+
+        $this->assertSame('php://memory', $app->get('logger')->getHandlers()[0]->getUrl());
     }
 
     /** @test */
