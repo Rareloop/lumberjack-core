90831d1f114a3ad4db2dc399b5b531cd692e9300
Adam Tomat <adamtomat@users.noreply.github.com>
2018-03-16 11:25:30 +0000

    Add filter before we compile a TimberResponse
    Some context: I wanted to add the twig template used as a class to the body. This is the only place that knows about both.

--- src/Http/Responses/TimberResponse.php
+++ src/Http/Responses/TimberResponse.php
@@ -10,6 +10,8 @@ class TimberResponse extends HtmlResponse
 {
     public function __construct($twigTemplate, $context, $status = 200, array $headers = [])
     {
+        $context = apply_filters('timber_response_before_compile', $context, $twigTemplate, $status, $headers);
+        
         $template = Timber::compile($twigTemplate, $context);
 
         if ($template === false) {
