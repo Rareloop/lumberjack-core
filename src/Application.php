213f3a963abae7575c522aa61344f4c52e7e2aab
Joe Lambert <joe@joelambert.co.uk>
2018-06-07 17:28:05 +0100

    Prevent duplicate headers being sent
    This was an issue experienced on MAMP PRO where duplicate headers in the response was causing an Internal Server Error.
    
    Unfortunately, it’s not possible to put tests around this as `headers_list()` does not work in the CLI. We may need to catch this at an integration level in the future.

--- src/Application.php
+++ src/Application.php
@@ -173,9 +173,36 @@ class Application implements ContainerInterface, InteropContainerInterface
             global $wp;
             $wp->send_headers();
 
-            send($response);
+            // If we're handling a WordPressController response at this point then WordPress will already have
+            // sent headers as it happens earlier in the lifecycle. For this scenario we need to do a bit more
+            // work to make sure that duplicate headers are not sent back.
+            send($this->removeSentHeadersAndMoveIntoResponse($response));
         }
 
         die();
+    }
+
+    protected function removeSentHeadersAndMoveIntoResponse(ResponseInterface $response) : ResponseInterface
+    {
+        // 1. Format the previously sent headers into an array of [key, value]
+        // 2. Remove all headers from the output that we find
+        // 3. Filter out any headers that would clash with those already in the response
+        $headersToAdd = collect(headers_list())->map(function ($header) {
+            $parts = explode(':', $header, 2);
+            header_remove($parts[0]);
+
+            return $parts;
+        })->filter(function ($header) {
+            return !in_array(strtolower($header[0]), ['content-type']);
+        });
+
+        // Add the previously sent headers into the response
+        // Note: You can't mutate a response so we need to use the reduce to end up with a response
+        // object with all the correct headers
+        $responseToSend = collect($headersToAdd)->reduce(function ($newResponse, $header) {
+            return $newResponse->withAddedHeader($header[0], $header[1]);
+        }, $response);
+
+        return $responseToSend;
     }
 }
