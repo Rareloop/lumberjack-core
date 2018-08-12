1fd63d9306f362425cf6a591f0048dee8df55d4f
Joe Lambert <joe@joelambert.co.uk>
2018-08-12 14:52:57 +0100

    Refactor Config to not use Symfony Finder

--- src/Config.php
+++ src/Config.php
@@ -30,13 +30,12 @@ class Config
 
     public function load(string $path) : Config
     {
-        $finder = new Finder();
-        $finder->files()->in($path)->name('/\.php$/');
+        $files = glob($path . '/*.php');
 
-        foreach ($finder as $file) {
-            $configData = include $file->getRealPath();
+        foreach ($files as $file) {
+            $configData = include $file;
 
-            $this->data[$file->getBasename('.php')] = $configData;
+            $this->data[pathinfo($file)['filename']] = $configData;
         }
 
         return $this;
