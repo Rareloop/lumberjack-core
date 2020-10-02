92c93823d30a530001c9369164577d9ef94fc0ae
David Mignot <197418+idflood@users.noreply.github.com>
2020-10-02 12:00:51 +0200

    Fix indentation

--- src/Providers/CustomTaxonomyServiceProvider.php
+++ src/Providers/CustomTaxonomyServiceProvider.php
@@ -4,12 +4,14 @@ namespace Rareloop\Lumberjack\Providers;
 
 use Rareloop\Lumberjack\Config;
 
-class CustomTaxonomyServiceProvider extends ServiceProvider {
-  public function boot(Config $config) {
-    $taxonomiesToRegister = $config->get('taxonomies.register');
+class CustomTaxonomyServiceProvider extends ServiceProvider
+{
+    public function boot(Config $config)
+    {
+        $taxonomiesToRegister = $config->get('taxonomies.register');
 
-    foreach ($taxonomiesToRegister as $taxonomy) {
-      $taxonomy::register();
+        foreach ($taxonomiesToRegister as $taxonomy) {
+            $taxonomy::register();
+        }
     }
-  }
 }
