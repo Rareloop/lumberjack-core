4222a3236309b718b85756397298b68a19cdd64e
Adam Tomat <adam.tomat@rareloop.com>
2025-10-09 17:08:46 +0100

    Update EncryptionServiceProvider
    This ensures that the encrypter is only created at the point of it being used.

--- src/Providers/EncryptionServiceProvider.php
+++ src/Providers/EncryptionServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace Rareloop\Lumberjack\Providers;
 
-use Rareloop\Lumberjack\Application;
 use Rareloop\Lumberjack\Contracts\Encrypter as EncrypterContract;
 use Rareloop\Lumberjack\Encrypter;
-use Rareloop\Lumberjack\Facades\Config;
-use Rareloop\Lumberjack\Session\SessionManager;
 
 class EncryptionServiceProvider extends ServiceProvider
 {
@@ -17,10 +14,30 @@ class EncryptionServiceProvider extends ServiceProvider
         if ($this->app->has('config')) {
             $encryptionKey = $this->app->get('config')->get('app.key');
 
-            $encrypter = new Encrypter($encryptionKey);
+            $encrypter = function () use ($config) {
+                $key = $config->get('app.key');
+                $cipher = $config->get('app.cipher', 'aes-256-cbc');
+
+                // Throw own exception
+                // Add unit test for legacy decryption
+                // test base64 and nonbase64
+
+                return new Encrypter($this->parseKey($key), $cipher);
+            };
 
             $this->app->bind(EncrypterContract::class, $encrypter);
             $this->app->bind('encrypter', $encrypter);
         }
+    }
+
+    protected function parseKey(string $key): string
+    {
+        $prefix = 'base64:';
+
+        if (str_starts_with($key, $prefix)) {
+            $key = base64_decode(str_replace($prefix, '', $key));
+        }
+
+        return $key;
     }
 }
