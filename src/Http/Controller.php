d9ee4c7114807b40ed1dd7a367b8c49c4dfec490
Tom Mitchelmore <tom.mitchelmore@rareloop.com>
2026-05-19 16:55:43 +0100

    Revert controller change

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -2,23 +2,8 @@
 
 namespace Rareloop\Lumberjack\Http;
 
-use Rareloop\Lumberjack\Helpers;
-use Rareloop\Lumberjack\Http\Responses\TimberResponse;
 use Rareloop\Router\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    /**
-     * Return a new TimberResponse from the controller.
-     *
-     * @param string $template
-     * @param array|\Illuminate\Contracts\Support\Arrayable $context
-     * @param integer $status
-     * @param array $headers
-     * @return \Rareloop\Lumberjack\Http\Responses\TimberResponse
-     */
-    public function view(string $template, $context = [], int $status = 200, array $headers = [])
-    {
-        return Helpers::view($template, $context, $status, $headers);
-    }
 }
