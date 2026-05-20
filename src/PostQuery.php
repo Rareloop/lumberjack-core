ec23d81023ebc11e9888fa1420ab0b23367bf837
Tom Mitchelmore <tom.mitchelmore@rareloop.com>
2026-05-20 11:23:36 +0100

    Remove toCollection from PostQuery

--- src/PostQuery.php
+++ src/PostQuery.php
@@ -9,14 +9,4 @@ use Timber\PostQuery as TimberPostQuery;
 class PostQuery extends TimberPostQuery
 {
     use Macroable;
-
-    /**
-     * Get the posts in this query as a Collection.
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function toCollection(): Collection
-    {
-        return new Collection($this->getArrayCopy());
-    }
 }
