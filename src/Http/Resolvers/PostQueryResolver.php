be3ebbb09d9c723f709506fe4dca5cb04507ccc3
Tom Mitchelmore <tom.mitchelmore@rareloop.com>
2026-05-20 11:15:21 +0100

    Update Timber import

--- src/Http/Resolvers/PostQueryResolver.php
+++ src/Http/Resolvers/PostQueryResolver.php
@@ -3,22 +3,20 @@
 namespace Rareloop\Lumberjack\Http\Resolvers;
 
 use Rareloop\Lumberjack\Application;
-use Timber\PostQuery as TimberPostQuery;
+use Timber\PostQuery;
 use Timber\PostCollectionInterface;
 use Timber\Timber;
 use WP_Query;
 
 class PostQueryResolver extends AbstractContextResolver
 {
-    public function __construct(protected Application $app)
-    {
-    }
+    public function __construct(protected Application $app) {}
 
     protected function canResolveClass(string $className): bool
     {
-        return $className === TimberPostQuery::class
+        return $className === PostQuery::class
             || $className === PostCollectionInterface::class
-            || is_subclass_of($className, TimberPostQuery::class);
+            || is_subclass_of($className, PostQuery::class);
     }
 
     protected function isValidContext(mixed $context, string $className): bool
@@ -34,7 +32,7 @@ class PostQueryResolver extends AbstractContextResolver
     protected function resolveObject(string $className, mixed $context): mixed
     {
         // If they asked for the interface or the base Timber PostQuery, use the factory
-        if ($className === PostCollectionInterface::class || $className === TimberPostQuery::class) {
+        if ($className === PostCollectionInterface::class || $className === PostQuery::class) {
             return Timber::get_posts($context);
         }
 
