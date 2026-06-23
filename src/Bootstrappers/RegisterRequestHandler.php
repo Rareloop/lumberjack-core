6613a54530702a60dc056e6bae5def04986b0dab
Tom Mitchelmore <tom.mitchelmore@rareloop.com>
2026-06-23 13:36:42 +0100

    Add defensive container check around bound request

--- src/Bootstrappers/RegisterRequestHandler.php
+++ src/Bootstrappers/RegisterRequestHandler.php
@@ -3,6 +3,10 @@
 namespace Rareloop\Lumberjack\Bootstrappers;
 
 use Rareloop\Lumberjack\Application;
+use Rareloop\Lumberjack\Http\ServerRequest;
+use Laminas\Diactoros\ServerRequestFactory;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class RegisterRequestHandler
 {
@@ -12,6 +16,21 @@ class RegisterRequestHandler
 
         if ($config->get('app.debug')) {
             $app->detectWhenRequestHasNotBeenHandled();
+        }
+
+        if (!$app->has('request')) {
+            $request = ServerRequest::fromRequest(ServerRequestFactory::fromGlobals(
+                $_SERVER,
+                $_GET,
+                $_POST,
+                $_COOKIE,
+                $_FILES
+            ));
+
+            $app->bind('request', $request);
+            $app->bind(RequestInterface::class, $request);
+            $app->bind(ServerRequestInterface::class, $request);
+            $app->bind(ServerRequest::class, $request);
         }
 
         $app->bind(\WP_Query::class, function () {
